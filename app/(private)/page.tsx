88ebd86df37575bc2efd11dcfcd1f3a5c293d46f
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-05 18:02:12 +0900

    canary versionに変更

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -3,8 +3,8 @@
 import {createClientComponentClient} from '@supabase/auth-helpers-nextjs'
 import axios from "axios";
 import {useEffect, useState} from "react";
-import NavigationBar from "@/components/nav/NavigationBar";
 import useUserStore from "@/store/user";
+import NavigationBar from "@/components/nav/NavigationBar";
 
 type backendRes = {
   servers: [{
@@ -17,36 +17,40 @@ type backendRes = {
 // 管理できるサーバーの一覧を表示します
 export default function Index() {
   const supabase = createClientComponentClient()
-  const [loading, setLoading] = useState<boolean>(true)
   const [backend, setBackend] = useState<backendRes>()
   const store = useUserStore()
 
   useEffect(() => {
     store.initialize()
-
-    supabase.auth.onAuthStateChange(
-      (event, session) => {
-        if (event === 'SIGNED_IN') {
-          // storeにログインユーザーを追加します
-          store.setLoginUserId(session?.user.id || "");
-          // backendからサーバーの一覧を取得します
-          if (session?.provider_token && !backend?.servers[0].id) {
-            console.log("リクエストを送信します")
-            const url = `${process.env.NEXT_PUBLIC_BE_URL!}/api/guild`
-            axios.get(url, {
-              headers: {
-                "Authorization": `Bearer ${session.provider_token}`
-              }
-            }).then((res) => {
-              setBackend(res.data)
-            })
-          }
-        } else if (event === 'SIGNED_OUT') {
-          store.setLoginUserId("");
-        }
-      }
-    );
   }, [])
+
+  useEffect(() => {
+    // backendからサーバーの一覧を取得します
+    supabase.auth.getSession().then(({data: {session}}) => {
+      console.log("token: ", session?.provider_token)
+      if (session?.provider_token && !backend?.servers[0].id) {
+        const url = `${process.env.NEXT_PUBLIC_BE_URL!}/api/guild`
+        axios.get(url, {
+          headers: {
+            "Authorization": `Bearer ${session.provider_token}`
+          }
+        }).then((res) => {
+          setBackend(res.data)
+        })
+      }
+    })
+  }, [store.loginUserId])
+
+  supabase.auth.onAuthStateChange(
+    (event, session) => {
+      if (event === 'SIGNED_IN') {
+        // storeにログインユーザーを追加します
+        store.setLoginUserId(session?.user.id || "");
+      } else if (event === 'SIGNED_OUT') {
+        store.setLoginUserId("");
+      }
+    }
+  );
 
   return (
     <div className="">
