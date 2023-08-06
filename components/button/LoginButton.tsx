a7baa2df70c59437b978a9f8b596cb08cfa30364
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-06 17:38:07 +0900

    warningのボタンの初期表示を修正

--- components/button/LoginButton.tsx
+++ components/button/LoginButton.tsx
@@ -30,7 +30,7 @@ export default function LoginButton() {
 
   return (
     <>
-      {loginLoading ? "" : loginUserId ? (
+      {loginLoading ? "..." : loginUserId ? (
         <button
           type="button"
           className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
