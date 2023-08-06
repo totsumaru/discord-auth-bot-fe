b91777b888b635ab4f0d2f21ace6f2c3d19b8b28
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-06 16:23:26 +0900

    トグルを変更できないようにした

--- components/table/RolesTable.tsx
+++ components/table/RolesTable.tsx
@@ -21,8 +21,8 @@ export default function RolesTable({roles}: Props) {
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="mt-10 sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">サーバー全体の権限</h1>
-          <p className="mt-2 text-sm text-gray-700">
+          <h1 className="text-base text-2xl font-semibold leading-6 text-gray-900">サーバー全体の権限</h1>
+          <p className="ml-1 mt-2 text-sm text-gray-700">
             各ロールのデフォルトの権限です。
           </p>
 
@@ -63,13 +63,16 @@ export default function RolesTable({roles}: Props) {
               setDescriptionDisplay(state => !state)
             }}
           >
-            {descriptionDisplay ? "説明を閉じる" : "ロールの説明を表示"}
+            {descriptionDisplay ? "ロールの説明を閉じる" : "ロールの説明を表示"}
           </button>
         </div>
       </div>
       <div className="mt-4 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <p className="ml-1 mt-2 text-sm text-gray-700">
+              ※権限の変更はできません
+            </p>
             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
               <div style={{maxHeight: '700px', overflowY: 'auto'}}>
                 <table className="min-w-full divide-y divide-gray-300">
--- components/toggle/TableToggle.tsx
+++ components/toggle/TableToggle.tsx
@@ -12,10 +12,10 @@ export default function TableToggle({enabled}: Props) {
   return (
     <Switch
       checked={enabled}
-      // onChange={setEnabled}
+      onChange={() => {}}
       className={classNames(
         enabled ? 'bg-indigo-600' : 'bg-gray-200',
-        'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2'
+        'pointer-events-none relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2'
       )}
     >
       <span className="sr-only">Use setting</span>
