b6d33111143a31f2cc22f63f8d563849ca3eb846
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-06 10:43:33 +0900

    ボタンのデザインを修正

--- components/table/RolesTable.tsx
+++ components/table/RolesTable.tsx
@@ -24,7 +24,7 @@ export default function RolesTable({roles}: Props) {
         </div>
         <button
           type="button"
-          className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           onClick={() => {
             setDescriptionDisplay(state => !state)
           }}
