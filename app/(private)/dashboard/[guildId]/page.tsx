42ace3fa8c8e08155f5ecf635c0f549d840b1d30
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-06 16:05:47 +0900

    formatを実行

--- app/(private)/dashboard/[guildId]/page.tsx
+++ app/(private)/dashboard/[guildId]/page.tsx
@@ -6,33 +6,7 @@ import useUserStore from "@/store/user";
 import axios from "axios";
 import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
 import {role} from "@/app/(private)/dashboard/[guildId]/backend_res";
-import {
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-} from '@heroicons/react/24/outline'
 import RolesTable from "@/components/table/RolesTable";
-
-const navigation = [
-  {name: 'Dashboard', href: '#', icon: HomeIcon, current: true},
-  {name: 'Team', href: '#', icon: UsersIcon, current: false},
-  {name: 'Projects', href: '#', icon: FolderIcon, current: false},
-  {name: 'Calendar', href: '#', icon: CalendarIcon, current: false},
-  {name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false},
-  {name: 'Reports', href: '#', icon: ChartPieIcon, current: false},
-]
-const teams = [
-  {id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false},
-  {id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false},
-  {id: 3, name: 'Workcation', href: '#', initial: 'W', current: false},
-]
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ')
-}
 
 export default function Index({
   params: {guildId}
@@ -42,7 +16,6 @@ export default function Index({
   const supabase = createClientComponentClient()
   const store = useUserStore()
   const [roles, setRoles] = useState<role[]>([])
-  const [sidebarOpen, setSidebarOpen] = useState(false)
 
   useEffect(() => {
     store.initialize()
