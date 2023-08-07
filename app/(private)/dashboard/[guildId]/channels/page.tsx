14ee3ee41c9d1f0718d06a8dd945728f371a8e49
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-07 18:41:12 +0900

    importを修正

--- app/(private)/dashboard/[guildId]/channels/page.tsx
+++ app/(private)/dashboard/[guildId]/channels/page.tsx
@@ -4,7 +4,6 @@
 import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
 import useUserStore from "@/store/user";
 import React, {useEffect, useState} from "react";
-import {channel, role} from "@/utils/backend_res_type";
 import Spinner from "@/components/loading/Spinner";
 import NavigationBar from "@/components/nav/NavigationBar";
 import ReturnTopButton from "@/components/button/ReturnTopButton";
@@ -16,6 +15,7 @@ import RolesTable from "@/components/table/RolesTable";
 import TopClientLayout from "@/components/layout/TopClientLayout";
 import {GetChannelList} from "@/utils/api/channel/list/list";
 import {GetChannel} from "@/utils/api/channel/channel";
+import {channel, role} from "@/utils/backend_res_type";
 
 export default function Index({
   params: {guildId}
