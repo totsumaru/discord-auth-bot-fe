8b96af163048725b462c3828ab7fd79f94666b11
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-09 14:41:37 +0900

    nav-barの不要な記述を削除

--- components/nav/NavigationBar.tsx
+++ components/nav/NavigationBar.tsx
@@ -1,11 +1,9 @@
 "use client"
 
-import {Fragment} from 'react'
-import {Disclosure, Menu, Transition} from '@headlessui/react'
+import {Disclosure} from '@headlessui/react'
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
 import LoginButton from "@/components/button/LoginButton";
 import Link from "next/link";
-import {classNames} from "@/utils/class_names";
 
 const tabClassFocus = "inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
 const tabClassNotFocus = "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
@@ -77,37 +75,8 @@ export default function NavigationBar({tabVisible, guildId, isServer}: Props) {
               {/* SP/PC 右側の内容 */}
               <div
                 className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* プロフィールのドロップダウン */}
-                <Menu as="div" className="relative ml-3">
-                  {/* ログインボタン */}
-                  <LoginButton/>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items
-                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({active}) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            onClick={() => {
-                            }}
-                          >
-                            ログアウト
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-
+                {/* ログインボタン */}
+                <LoginButton/>
               </div>
             </div>
           </div>
