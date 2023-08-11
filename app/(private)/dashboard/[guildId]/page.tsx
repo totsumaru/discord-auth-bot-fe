44dbda95a99eba9b0ab566ae575a7c13c5e8fdcf
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-11 14:49:32 +0900

    serverのロール一覧をSSRに変更

--- app/(private)/dashboard/[guildId]/page.tsx
+++ app/(private)/dashboard/[guildId]/page.tsx
@@ -1,65 +1,41 @@
-"use client"
-
 import NavigationBar from "@/components/nav/NavigationBar";
-import React, {useEffect, useState} from "react";
-import useUserStore from "@/store/user";
-import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import React from "react";
 import RolesTable from "@/components/table/RolesTable";
-import Spinner from "@/components/loading/Spinner";
 import DashboardContentLayout from "@/components/layout/DashboardContentLayout";
 import Heading from "@/components/section/Heading";
-import TopClientLayout from "@/components/layout/TopClientLayout";
 import {GetAllRoles} from "@/utils/api/server/server";
-import {guild, role} from "@/utils/backend_res_type";
-import LoginSection from "@/components/section/LoginSection";
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
 
-export default function Index({
+export default async function Index({
   params: {guildId}
 }: {
   params: { guildId: string }
 }) {
-  const supabase = createClientComponentClient()
-  const store = useUserStore()
-  const [roles, setRoles] = useState<role[]>([])
-  const [guildInfo, setGuild] = useState<guild>()
+  const supabase = createServerComponentClient({cookies})
+  const {data: {session}} = await supabase.auth.getSession()
 
-  useEffect(() => {
-    // backendからサーバー全体のロールの権限を取得します
-    supabase.auth.getSession().then(({data: {session}}) => {
-      if (session) {
-        GetAllRoles({accessToken: session.access_token, guildId: guildId})
-          .then((res) => {
-            // 全ロールを保存します
-            setRoles(res.roles)
-            setGuild(res.server)
-          })
-          .catch(e => console.error(e))
-      }
-    })
-  }, [store.loginUserId])
+  const {roles, server} = await GetAllRoles({
+    accessToken: session?.access_token || "",
+    guildId: guildId,
+  })
 
   return (
-    <TopClientLayout>
-      <div className="min-h-screen bg-gradient_1 bg-cover bg-center">
-        <NavigationBar guildId={guildId} focusTab="server"/>
-        {store.loginLoading ? (
-          <Spinner/>
-        ) : (
-          store.loginUserId ? (
-            <DashboardContentLayout>
-              <Heading
-                title={"サーバー全体の権限"}
-                content={"各ロールのデフォルトの権限です。"}
-                serverName={guildInfo?.name ?? ""}
-                serverIconUrl={guildInfo?.icon_url ?? ""}
-              />
-              <RolesTable roles={roles} tableType={"server"}/>
-            </DashboardContentLayout>
-          ) : (
-            <LoginSection/>
-          )
-        )}
-      </div>
-    </TopClientLayout>
+    <div className="min-h-screen bg-gradient_1 bg-cover bg-center">
+      <NavigationBar
+        guildId={guildId}
+        focusTab="server"
+        accessToken={session?.access_token || ""}
+      />
+      <DashboardContentLayout>
+        <Heading
+          title={"サーバー全体の権限"}
+          content={"各ロールのデフォルトの権限です。"}
+          serverName={server.name ?? ""}
+          serverIconUrl={server.icon_url ?? ""}
+        />
+        <RolesTable roles={roles} tableType={"server"}/>
+      </DashboardContentLayout>
+    </div>
   )
 }
