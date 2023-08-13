4964b186dfb87df3778e15f7c717a440ba0fbf5f
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-13 16:16:37 +0900

    successページを作成

---
+++ app/dashboard/[guildId]/success/page.tsx
@@ -0,0 +1,28 @@
+export default async function Index({
+  params: {guildId}
+}: {
+  params: { guildId: string }
+}) {
+  return (
+    <div className="min-h-screen bg-gradient_1 bg-cover bg-center">
+      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            プランがProへと変更されました🎉
+          </h2>
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
+            ダッシュボードから、全てのチャンネルの権限を確認できるようになりました。
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href={`/dashboard/${guildId}`}
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              ダッシュボードへ戻る
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/section/Payment.tsx
+++ components/section/Payment.tsx
@@ -38,7 +38,9 @@ export default function Payment({
         ? subscriber?.id === loginUser.id
           ? (
             <>
-              <p>あなたによってこのサーバーはProプランとなっています。<br/>支払い情報は決済者本人しか確認できません</p>
+              <p>あなたによってこのサーバーはProプランとなっています。
+                <br/>支払い情報は決済者本人しか閲覧・確認できません
+              </p>
               <button
                 type="button"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
