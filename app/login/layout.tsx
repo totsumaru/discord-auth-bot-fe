faf4accb6264c1663a583fd5f382aa5db85b84a7
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-16 14:58:20 +0900

    背景を修正

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -6,7 +6,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="min-h-screen bg-gradient_1 bg-auto bg-center">
+    <div className="min-h-screen bg-gradient_1 bg-cover bg-center">
       {children}
     </div>
   )
