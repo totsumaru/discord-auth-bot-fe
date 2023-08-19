af6ca33b0581b351b2c6aa51e41ff8d59fca1d69
totsuka <techstart35@gmail.com>
2023-08-19 16:35:56 +0900

    画像を変更

--- app/page.tsx
+++ app/page.tsx
@@ -78,8 +78,10 @@ export default function Index() {
               <div
                 className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <img
-                  src="/dashboard.png"
+                  src="/dashboard.webp"
                   alt="App screenshot"
+                  width={2432}
+                  height={1442}
                   className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
                 />
               </div>
